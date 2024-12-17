1a8673b5f4a69b6a7641a5e78f8975de7498aa79
Trần Lê Công Ti <congti1812@gmail.com>
2024-12-17 16:33:47 +0700

    ti up code

--- src/main/java/com/book/bookapplication/controller/HomeController.java
+++ src/main/java/com/book/bookapplication/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.book.bookapplication.controller;
 
+import com.book.bookapplication.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/home")
-    public String Home() {
+    public String Home(Model model) {
+        model.addAttribute("category_list", categoryService.findAllCategory());
+        System.out.println(categoryService.findAllCategory());
         return "index";
     }
 
